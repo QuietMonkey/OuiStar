b1e5470c13040c0432e3bdff48d66fe7351f0c32
QuietMonkey <taillandierbertrand@gmail.com>
2019-09-11 14:23:37 +0200

    get vehicles in one unique array

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
 import './App.css'
 
-class App extends Component{
-  render(){
-  return (
-    <div className="App">
-      <h1>Clean</h1>
-    </div>
-  )
-}
+class App extends Component {
+  state = {
+    vehicles : []
+  }
+
+  getVehicles = async() => {
+    try {
+      let arrayResults = []
+      const response = await axios.get('https://swapi.co/api/vehicles/?page=1')
+      this.createUnikArray(response.data.results, arrayResults)
+
+      const response2 = await axios.get('https://swapi.co/api/vehicles/?page=2')
+      this.createUnikArray(response2.data.results, arrayResults)
+
+      const response3 = await axios.get('https://swapi.co/api/vehicles/?page=3')
+      this.createUnikArray(response3.data.results, arrayResults)
+
+      const response4 = await axios.get('https://swapi.co/api/vehicles/?page=4')
+      this.createUnikArray(response4.data.results, arrayResults)
+
+      console.log(arrayResults)
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  createUnikArray = (data, array) => {
+    data.map((one)=> array.push(one))
+  }
+
+  componentDidMount(){
+    this.getVehicles()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Clean</h1>
+      </div>
+    )
+  }
 }
 
 export default App
